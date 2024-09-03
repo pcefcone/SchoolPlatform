55ee7369f85a2514ee6dcd1307ff9ff8ba8f1724
Yaroslav <yaroslav.kerez@gmail.com>
2024-09-03 16:43:55 +0200

    add struct +add functions

---
+++ SchoolPlatform/Admin/AddClass.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolPlatform.Models.CommonFunction;
+
+namespace SchoolPlatform.Admin
+{
+    public partial class AddClass1 : System.Web.UI.Page
+    {
+        CommonFuuc
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetClass()
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
